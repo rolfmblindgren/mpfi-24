8e9b81c72a3f7fb33f25119116abac135ab9726a
Rolf Marvin Bøe Lindgren <roffe@Rolfs-MacBook-Pro.local>
2021-12-15 10:58:40 +0100

    First push

---
+++ app.R
@@ -0,0 +1,80 @@
+library(shiny)
+library(shiny.semantic)
+library(ggplot2)
+
+grid_charts <- grid_template(
+    default = list(areas = rbind(c("chart1"#, "chart2"
+                                   )),
+                   rows_height = c("100%","100%"),
+                   cols_width = c("85.71429%", "14.28571%"))
+)
+
+my_data <- read.csv("https://eu.jotform.com/csv/213463841840051")
+my_data <- my_data[which(!is.na(my_data[["Klientens.id"]])),]
+
+my_scales <- c(31:42)
+my_scales_f <- c(31:36)
+my_scales_r <- c(37:42)
+
+ui <- semanticPage(
+    title = "MPFI 24-kalkulator",
+    h1("Kalkulatoren"),
+    sidebar_layout(
+        sidebar_panel(
+            dropdown_input(
+                "simple_dropdown",
+                my_data["Klientens.id"],
+                type = "search selection"),
+            p("Selected client:"),
+            textOutput("selected_client")), 
+        main_panel(
+            grid(grid_charts,
+                 chart1 = plotOutput("plot1")#,chart2 = plotOutput("plot2")
+                 )
+        )
+    )
+)
+
+server <- shinyServer(function(input, output, session) {
+
+    output$selected_client <-
+        renderText({
+        
+            
+            a <- input[["simple_dropdown"]]
+
+            paste(a, collapse = ", ")
+
+        })
+
+    output$plot1 <- renderPlot({
+
+        update_dropdown_input(session, "simple_dropdown")
+            
+        my_data <- my_data[which(my_data[,"Klientens.id"]==session$input$simple_dropdown),]
+            
+        df <- data.frame(
+            Skala <- c(colnames(my_data[my_scales]),
+                       c("","Fleksibilitet","Rigiditet")),
+            Skår <- c(as.vector(unlist(my_data[my_scales])),NA,
+                      c(mean(as.numeric(my_data[my_scales_f])),
+                        mean(as.numeric(my_data[my_scales_r])))))
+        
+        colnames(df) <- c("Skala","Skår")
+                                        #        df$Skala <- factor(df$Skala, levels = df$Skala[order(df$Skår)])
+        df$Skala <- factor(df$Skala, levels = df$Skala)
+
+       
+        ggplot(df,aes(x=Skala,y=Skår,fill=Skår)) +
+            theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1),
+                  legend.position="none") +
+            geom_bar(stat = "identity")
+        
+        
+    })
+
+})
+
+
+
+shinyApp(ui = ui, server = server)
